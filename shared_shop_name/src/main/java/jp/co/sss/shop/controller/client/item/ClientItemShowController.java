9077213e2a1b58497eecc35f64ccf06478a390c5
saito <aaa@bbb.co.jp>
2024-12-12 10:34:09 +0900

    ClientItemShowControllerとdetail.htmlの更新

--- shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -3,10 +3,15 @@ package jp.co.sss.shop.controller.client.item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.repository.ItemRepository;
+import jp.co.sss.shop.repository.UserRepository;
 import jp.co.sss.shop.service.BeanTools;
 
 /**
@@ -15,19 +20,28 @@ import jp.co.sss.shop.service.BeanTools;
  * @author SystemShared
  */
 @Controller
+
 public class ClientItemShowController {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	HttpSession session;
+	
 	/**
 	 * 商品情報
 	 */
-	@Autowired
-	ItemRepository itemRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
 
 	/**
 	 * Entity、Form、Bean間のデータコピーサービス
 	 */
 	@Autowired
 	BeanTools beanTools;
-	
+
 	/**
 	 * トップ画面 表示処理
 	 *
@@ -39,4 +53,24 @@ public class ClientItemShowController {
 	
 		return "index";
 	}
+	
+	
+
+	/**
+	 * 商品名リンククリック→詳細表示
+	 */
+	
+	@GetMapping("/client/item/detail/{id}")
+	public String detail(@PathVariable Integer id, Model model){
+		Item item = itemRepository.getReferenceById(id);
+		model.addAttribute("item", item);
+		return "client/item/detail";
+	}
+	
+	
+
+
+
+	
+
 }
