e1edda48e350c327d3bad4e0fc3e073038de0e85
horikawayuka <aaa@bbb.co.jp>
2024-12-13 15:07:23 +0900

    追加 service/CatchItemListOnSortService.java
    更新
    ClientItemShowController.java
    ItemRepository.java
    BeanTools.java
    ItemBeanComparator.java
    list.html
    sidebar.html

--- shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -17,6 +17,8 @@ import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.repository.ItemRepository;
 import jp.co.sss.shop.repository.UserRepository;
 import jp.co.sss.shop.service.BeanTools;
+import jp.co.sss.shop.service.CatchItemListOnSortService;
+import jp.co.sss.shop.util.Constant;
 import jp.co.sss.shop.util.ItemBeanComparator;
 
 /**
@@ -33,19 +35,24 @@ public class ClientItemShowController {
 
 	@Autowired
 	HttpSession session;
-	
+
 	/**
 	 * 商品情報
 	 */
-
-    @Autowired
-    ItemRepository itemRepository;
+	@Autowired
+	ItemRepository itemRepository;
 
 	/**
 	 * Entity、Form、Bean間のデータコピーサービス
 	 */
 	@Autowired
 	BeanTools beanTools;
+
+	/**
+	 * 商品一覧ソートサービス
+	 */
+	@Autowired
+	CatchItemListOnSortService catchItemListOnSortService;
 
 	/**
 	* トップ画面 表示処理
@@ -56,7 +63,8 @@ public class ClientItemShowController {
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String showTopPage(Model model) {
 		// 商品一覧を「売れ筋順」で取得
-		List<ItemBean> items = beanTools.copyEntityListToItemBeanList(itemRepository.findAllByDeleteFlag(0));
+		List<ItemBean> items = beanTools
+				.copyEntityListToItemBeanList(itemRepository.findAllByDeleteFlag(Constant.NOT_DELETED));
 		items.sort(new ItemBeanComparator());
 		int totalQuantity = 0;
 		for (ItemBean itemBean : items) {
@@ -84,96 +92,33 @@ public class ClientItemShowController {
 	 *
 	 * @param sortType   表示順 (1: 新着順, 2: 売れ筋順)
 	 * @param categoryId カテゴリーID (null または 0 の場合は全カテゴリ)
+	 * @param price 検索価格帯
 	 * @param model Viewとの値受渡し
 	 * @return 商品一覧画面
 	 */
 	@RequestMapping(path = "client/item/list/{sortType}", method = RequestMethod.GET)
 	public String clientItemList(@PathVariable("sortType") int sortType,
 			@RequestParam(value = "categoryId", required = false, defaultValue = "0") Integer categoryId,
+			@RequestParam(value = "price", required = false, defaultValue = "0") String price,
 			Model model) { //categoryId が0の場合は全カテゴリで表示されるように設定
-		List<ItemBean> items;
-		// カテゴリ検索の条件分岐
-		System.out.println("1");
-		if (categoryId != null && categoryId != 0) {
-			if (sortType == 1) {
-				// カテゴリ指定の新着順
-				System.out.println("2");
-				items = beanTools.getItemsByCategorySortedByLatest(categoryId);
-			} else if (sortType == 2) {
-				// カテゴリ指定の売れ筋順
-				System.out.println("3");
-				List<Item> categoryItems = itemRepository.findByCategoryIdAndDeleteFlag(categoryId, 0); // 該当カテゴリのアイテム取得
-	            items = beanTools.copyEntityListToItemBeanList(categoryItems); // アイテムをBeanに変換
-	            items.sort(new ItemBeanComparator()); // 売れ筋順に並び替え
-			} else {
-				// デフォルト：カテゴリ指定の新着順
-				System.out.println("4");
-				items = beanTools.getItemsByCategorySortedByLatest(categoryId);
-			}
-		} else {
-			if (sortType == 1) {
-				// 全カテゴリの新着順
-				System.out.println("5");
-				items = beanTools.getNewItems();
-			} else if (sortType == 2) {
-				// 全カテゴリの売れ筋順
-				System.out.println("6");
-				items = beanTools.copyEntityListToItemBeanList(itemRepository.findAllByDeleteFlag(0));
-				items.sort(new ItemBeanComparator());
-			} else {
-				// デフォルト：全カテゴリの新着順
-				System.out.println("7");
-				items = beanTools.getNewItems();
-			}
-		}
+
 		// モデルにデータを設定
-		System.out.println("8");
-		model.addAttribute("items", items);
-		System.out.println("9");
+		model.addAttribute("items", catchItemListOnSortService.creatItemList(sortType, categoryId, price));
 		model.addAttribute("sortType", sortType);
-		System.out.println("10");
 		model.addAttribute("categoryId", categoryId);
-		System.out.println("11");
+		model.addAttribute("price", price);
 		return "client/item/list"; // 商品一覧画面
 	}
 
 	/**
 	 * 商品名リンククリック→詳細表示
 	 */
-	
+
 	@GetMapping("/client/item/detail/{id}")
-	public String detail(@PathVariable Integer id, Model model){
+	public String detail(@PathVariable Integer id, Model model) {
 		Item item = itemRepository.getReferenceById(id);
 		model.addAttribute("item", item);
 		return "client/item/detail";
-	}
-
-  
-  //　修正しないといけない
-  
-	/**
-	 * トップ画面　価格別検索
-	 * 
-	 * @param model　
-	 * @param price
-	 * @return "client/item/list" 一覧表示画面
-	 */
-	@GetMapping("/client/item/list/price")
-	public String showPrice(String price, Model model) {
-		//文字列を区別して条件検索を行う
-		if (price.equals("30000")) {
-			Integer over = Integer.parseInt(price);
-			
-			model.addAttribute("items", beanTools.copyEntityListToItemBeanList(itemRepository.findByOverPriceQuery(over)));
-		} else if (price.equals("0")) {
-			model.addAttribute("items", beanTools.copyEntityListToItemBeanList(itemRepository.findAllByOrderByPriceAsc()));
-		} else {
-			String[] prices = price.split("～");
-			Integer min = Integer.parseInt(prices[0]);
-			Integer max = Integer.parseInt(prices[1]);
-			model.addAttribute("items", beanTools.copyEntityListToItemBeanList(itemRepository.findByPriceQuery(min, max)));
-		}
-		return "client/item/list";
 	}
 
 }
--- shared_shop_name/src/main/java/jp/co/sss/shop/repository/ItemRepository.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/repository/ItemRepository.java
@@ -49,15 +49,15 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 	 *新着順の商品一覧を取得
 	 * @return 商品エンティティ
 	 */
-	@Query("SELECT i FROM Item i ORDER BY i.insertDate DESC,i.id DESC")
-	List<Item> findNewItems();
+	@Query("SELECT i FROM Item i WHERE i.deleteFlag = :deleteFlag ORDER BY i.insertDate DESC,i.id DESC")
+	List<Item> findNewItems(@Param("deleteFlag") int deleteFlag);
 
 	/**
 	 * カテゴリ指定の新着順を検索
 	 * @param categoryId カテゴリーID
 	 * @return 商品エンティティ
 	 */
-	List<Item> findByCategoryIdOrderByInsertDateDesc(Integer categoryId);
+	List<Item> findByCategoryIdAndDeleteFlagOrderByInsertDateDesc(Integer categoryId,int deleteFlag);
 
 	/**
 	 * カテゴリ指定の売れ筋順を検索
@@ -79,20 +79,30 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 	 * @param price
 	 * @return 商品エンティティ
 	 */
-	@Query("SELECT i FROM Item i WHERE i.price >=:min AND i.price<=:max Order By i.price Asc")
-	public List<Item> findByPriceQuery(@Param("min") Integer min, @Param("max") Integer max);
+	@Query("SELECT i FROM Item i WHERE i.price >=:min AND i.price<=:max AND i.deleteFlag = :deleteFlag Order By i.price Asc")
+	public List<Item> findByPriceQuery(@Param("min") Integer min, @Param("max") Integer max,@Param("deleteFlag") int deleteFlag);
 
 	/**
 	 * 価格を条件に検索（\30000以上を選択後、条件を検索）
 	 * @param over
 	 * @return 商品エンティティ
 	 */
-	@Query("SELECT i FROM Item i WHERE i.price >=:over Order By i.price Asc")
-	public List<Item> findByOverPriceQuery(@Param("over") Integer over);
+	@Query("SELECT i FROM Item i WHERE i.price >=:over AND i.deleteFlag = :deleteFlag Order By i.price Asc")
+	public List<Item> findByOverPriceQuery(@Param("over") Integer over, @Param("deleteFlag") int deleteFlag);
 
 	/**
+	 * 多分つかわない
 	 * 価格を条件に検索（-指定なし-を選択後、全件検索）
 	 * @return 商品エンティティ
 	 */
 	List<Item> findAllByOrderByPriceAsc();
+	
+	//価格が30,000以上でカテゴリ検索（新着順）
+	@Query("SELECT i FROM Item i WHERE i.category.id = :categoryId AND i.price >= :over AND i.deleteFlag = :deleteFlag ORDER BY i.insertDate DESC, i.id DESC")
+	List<Item> findByCategoryIdAndPriceOverOrderByInsertDateDesc(@Param("categoryId") Integer categoryId, @Param("over") Integer price, @Param("deleteFlag") int deleteFlag);
+	// 価格が30,000以下でカテゴリ検索（新着順）
+	@Query("SELECT i FROM Item i WHERE i.category.id = :categoryId AND i.price >= :min AND i.price <= :max AND i.deleteFlag = :deleteFlag ORDER BY i.insertDate DESC, i.id DESC")
+	List<Item> findByCategoryIdAndPriceUnderOrderByInsertDateDesc(@Param("categoryId") Integer categoryId, @Param("min") Integer min, @Param("max") Integer max, @Param("deleteFlag") int deleteFlag);
+	
+	
 }
