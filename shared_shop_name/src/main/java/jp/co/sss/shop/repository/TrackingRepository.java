de95e330d686afbeb67a2257989b047a5b532e29
connor <connorketcheson01@gmail.com>
2024-12-20 14:20:43 +0900

    更新

--- shared_shop_name/src/main/java/jp/co/sss/shop/repository/TrackingRepository.java
+++ shared_shop_name/src/main/java/jp/co/sss/shop/repository/TrackingRepository.java
@@ -1,9 +1,14 @@
 package jp.co.sss.shop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import jp.co.sss.shop.entity.Tracking;
 
+/**
+ * trackingテーブル用リポジトリ
+ */
+@Repository
 public interface TrackingRepository extends JpaRepository<Tracking, Integer> {
 	Tracking findByOrderId(Integer orderId);
 }
